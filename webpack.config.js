b10d8618e9e74f028aa4d625822343cd6a2f3546
headlessNode <harriskhan047@outlook.com>
2025-03-16 14:10:10 +0500

    chore: add initial files

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require( 'path' );
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
